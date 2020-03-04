40b2b99e9f9970a5cf89fe4356f069043112d21c
alexarinho <56392527+alexarinho@users.noreply.github.com>
2020-03-04 20:17:41 +0100

    Update Agenda.java

--- Agenda/src/Agenda.java
+++ Agenda/src/Agenda.java
@@ -28,7 +28,7 @@ public class Agenda {
 		return numero;
 	}
 	/*
-	*
+	*Aqui mostramos por pantalla los contactos que tenemos almacenados en memoria 
 	*/
 	public static void verContactos(String mAgenda[][]) {
 		System.out.println("Tus contactos actuales son: ");
@@ -40,7 +40,9 @@ public class Agenda {
 		}
 		
 	}
-	
+	/*
+	*Este metodo sirve para buscar un espacio en el vector donde almacenamos los contactos.
+	*/
 	public static int espacio(String mAgenda[][]) {
 		int espacio=0;
 		for(int i=0;i<mAgenda.length;i++) {
@@ -51,7 +53,9 @@ public class Agenda {
 		}
 		return espacio;
 	}
-	
+	/*
+	*En este metodo guardaremos los contactos introduciendo un nombre y un numero de telefono
+	*/
 	public static void guardarContactos(String mAgenda[][]) {
 		int posicion=espacio(mAgenda);
 		Scanner leer;
@@ -62,19 +66,25 @@ public class Agenda {
 		mAgenda [posicion][1]=leer.nextLine();
 		
 	}
-	
+	/*
+	*Aqui daremos la opcion de borrar un contacto guardado pasando su posicion a null
+	*/
 	public static void borrarContactos(String mAgenda[][]) {
 		int borrrar=buscarContactos(mAgenda);
 		
 		mAgenda[borrrar][0]=null;
 		mAgenda[borrrar][1]=null;
 	}
-	
+	/*
+	*Aqui modificamos el contacto que tengamos guardado utilizando los metodos de borrar y guardar a la vez
+	*/
 	public static void modificarContactos(String mAgenda[][]) {
 		borrarContactos(mAgenda);
 		guardarContactos(mAgenda);
 	}
-	
+	/*
+	*Este metodo sirve para mostrarnos el contacto que queramos buscandolo en el vector
+	*/
 	public static int buscarContactos(String mAgenda[][]) {
 		Scanner leer;
 		leer = new Scanner(System.in);
@@ -89,6 +99,10 @@ public class Agenda {
 		}
 		return posi;
 	}
+	/*
+	*Aqui se llevara a cabo el switch en el que llamamos a nuestro menu y a todos los metodos anteriormente creados
+	para que nuestro programa se vea bien y funcione correctamente
+	*/
 	public static void main(String[] args) {
 		Scanner leer;
 		leer = new Scanner(System.in);
